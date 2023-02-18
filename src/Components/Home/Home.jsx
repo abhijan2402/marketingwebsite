fb345f06a18030ee3a12fd98234f0c0ab9fa29b9
DSRCODDE <daljeetkalyanwat62@gmail.com>
2023-02-18 19:29:20 +0530

    Hello

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import './Home.css'
+
+const Home = () => {
+  return (
+    <>
+        <div className="main_container">
+            <div className='main_content'> <Button variant="contained">Cute Cloud</Button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
