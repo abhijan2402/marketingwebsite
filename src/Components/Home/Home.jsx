e6fbfa7c903d2889baddd15cf0c9adb6493ca8ab
DSRCODDE <daljeetkalyanwat62@gmail.com>
2023-02-18 23:07:30 +0530

    test

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import './Home.css'
+import React from "react";
+import {Link} from 'react-router-dom';
+import Button from "@mui/material/Button";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-        <div className="main_container">
-            <div className='main_content'> <Button variant="contained">Cute Cloud</Button>
-            </div>
+      <div className="main_container">
+        <div className="Me_details">
+          <h5>Hello,</h5>
+          <h1>I'm Mark</h1>
+          <h6>Welcome to my site!</h6>
         </div>
+        <div className="me_details_skills">
+          <div className="me_details_skill1">
+            <h5>I'm a <span>minimalist</span></h5>
+            <h1>Designer</h1>
+          </div>
+          <div className="me_details_skill2">
+            <h5>I'm a  <span>creative</span></h5>
+            <h1>Developer</h1>
+          </div>
+        </div>
+        <div className="main_content">
+          <Link style={{textDecoration:"none"}} to="/PublicDetails">  <Button variant="contained">Cute Cloud</Button> </Link>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/Components/PublicDetails/PublicDetails.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import "./PublicDetails.css";
+import Button from '@mui/material/Button';
+import pic from "../../assest/bg_im.jpg";
+
+const PublicDetails = () => {
+  return (
+    <>
+      <div className="container_1">
+        <div className="container_2">
+          <div className="main_img">
+            <img src={pic} alt="" />
+          </div>
+          <div className="content">
+            <div className="main_heading">
+              <p>
+                Iris launches ‘Carbon kickback’ incentivising clients to fight
+                climate change
+              </p>
+              <h6>14 April 2021</h6>
+            </div>
+            <div className="main_content">
+              <p>
+                Iris has launched a new global initiative dubbed the ‘Carbon
+                kickback’ as part of its ongoing commitment to the planet. The
+                initiative encourages clients to sign up to a new ‘client
+                climate charter’ and incentivizes those who sign up with access
+                to the agency’s sustainability specialists free of charge. Iris
+                has set ambitious targets for its 15-office network to be
+                net-zero by 2025 and now the creative innovation agency wants to
+                help clients move forward in their eco journey too.
+              </p>
+              <img src={pic} alt="" />
+              <p>
+                A new climate charter encourages clients to take positive
+                climate action by committing to 5 key principles that will help
+                marketing departments contribute to the reduction of their
+                organisation’s overall carbon footprint. Iris will incentivize
+                clients by reinvesting any time that is saved back into their
+                business, in the form of free consultancy sessions with
+                sustainability specialists in their ‘Future Strategy’ team. The
+                Future Strategy team has already been working with big name
+                brands to develop more sustainable and socially valuable
+                strategies in areas such as circularity, social purpose, carbon
+                offsetting and EVs. The charter’s key focus is on rapid
+                reductions in international travel, which prior to lockdown made
+                up 55% of Iris’ global emissions. In 2020, Iris invested heavily
+                in collaboration tools to maximise the benefits of a
+                digital-first approach, and has developed a proprietary remote
+                working product that has been adopted by Iris’ B2B clients. Less
+                time travelling has meant more time dedicated to solving
+                client’s problems, and to help sustain this ‘digital-first’
+                approach post-lockdown, Iris has committed to protecting and
+                reinvesting any time saved from not travelling, directly back
+                into their client’s business. The agency network has set ‘carbon
+                budgets’ for each office and will measure its progress via its
+                inhouse data analysts. They will also invest in carbon credits
+                against any emissions that cannot be avoided or reduced. Travel
+                is just one of five key principles underpinning the climate
+                charter, others include; commitment to sustainable production
+                processes; promotion of sustainable behaviour in creative work
+                produced; commitment to reporting and measuring the carbon
+                impact of campaigns through ‘Ecoffectiveness’ and finally
+                propelling progress through participation - by encouraging other
+                agencies within the global advertising industry to adopt similar
+                models, a move which is backed by The Purpose Disruptors.
+                Starbucks has been named as the first agency-client partner to
+                back the charter, with Iris hoping more will follow. Fellow
+                networked agencies Cheil, Barbarian, BMB and McKinney have also
+                pledged their commitment to the progressive initiative. Ben
+                Essen, Global Chief Strategy Officer at Iris said: “In 2019,
+                $1.4 trillion was spent globally on business travel - but 2020
+                has shown us that we can work smarter to save time, money and
+                the planet.
+              </p>
+              <img src={pic} alt="" />
+              <p>
+                It's our job to empower clients to take bold action in the fight
+                against climate change. The carbon kickback is a tangible way
+                for us to start making a difference immediately, for both Iris
+                and our clients.” Reuben Arnold, Vice President of Marketing &
+                Product at Starbucks EMEA, commented: “I’ve experienced
+                first-hand the transformational effects of working with Iris’
+                Future Strategy team in shaping how we engage customers in new
+                ways, particularly in the last year and I’m proud to support the
+                client climate charter.” In addition to the client charter, Iris
+                has been pioneering internal policies to encourage wider carbon
+                footprint reduction, such as offering its people additional
+                holiday if they proactively choose to take the train or drive as
+                opposed to flying for personal holidays. They have also
+                collaborated with carbon measurement platform Giki to enable
+                each of their people around the world to personally contribute
+                to an overall reduction target. Iris has been at the forefront
+                of sustainability action within the advertising industry for the
+                past 3 years, leading initiatives such as Create & Strike in
+                2019, and working closely with The Purpose Disruptors to create
+                The Great Reset in 2020. Ben Essen, Iris’ Global Chief Strategy
+                Officer is a founding member of the Advertising Association’s
+                climate action working group, helping shape the strategy for ‘Ad
+                Net Zero’, the industry’s plan to reach net zero by 2030. As
+                part of the plan, the AA, IPA and ISBA encourage agencies to
+                take part in Ecoffectiveness, a new measurement tool created by
+                Ben and Elvis’ Caroline Davison, that encourages return on Co2
+                to become the new measure for effectiveness in advertising.
+              </p>
+              <img src={pic} alt="" />
+              <img src={pic} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="btn">
+          <Button variant="outlined">Learn More</Button>
+          <Link to="/" style={{textDecoration:"none"}}> <Button variant="outlined">Go Back</Button> </Link>
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default PublicDetails;
