06c3f9ea4f25beeef3a1c677a1a9ace1d470cd0e
Chirag <takchirag828@gmail.com>
2023-02-28 18:25:34 +0530

    home changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,15 +9,15 @@ const Home = () => {
         <div className="content">
           <div className="Me_details">
             <h5>Hello,</h5>
-            <h1>I'm Mark</h1>
-            <h6>Welcome to my site!</h6>
+            {/* <h1>I'm Mark</h1> */}
+            <h3>Welcome to modern day marketing</h3>
           </div>
           <div className="me_details_skills">
             <div className="me_details_skill1">
               <h5>
-                I'm a <span>minimalist</span>
+                I'm 
               </h5>
-              <h1>Designer</h1>
+              <h1>Real-estate</h1>
             </div>
             <Link
               to="/HomePage"
@@ -28,11 +28,11 @@ const Home = () => {
               }}
               className="developer"
             >
-              <div className="me_details_skill2">
+              <div className="me_details_skill2" style={{display:"flex",flexDirection: 'column',}}>
                 <h5>
-                  I'm a <span>creative</span>
+                  <span>Explore</span>
                 </h5>
-                <h1>Developer</h1>
+                <h1>Other Brands</h1>
               </div>
             </Link>
           </div>
